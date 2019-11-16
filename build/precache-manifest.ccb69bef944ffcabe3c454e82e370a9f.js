550c066158e148ea4d0724dc928438cd55816c26
Yaron <45236471+yaroncbtv@users.noreply.github.com>
2019-11-16 12:53:33 +0200

    Add files via upload

---
+++ build/precache-manifest.ccb69bef944ffcabe3c454e82e370a9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd89510ee1d5bf50a83f4cfe215d9aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b87a87662d435c0431",
+    "url": "/static/css/main.4b1de1e4.chunk.css"
+  },
+  {
+    "revision": "28ab8d5326b506d5038b",
+    "url": "/static/js/2.9e2d7295.chunk.js"
+  },
+  {
+    "revision": "39b87a87662d435c0431",
+    "url": "/static/js/main.debb8338.chunk.js"
+  },
+  {
+    "revision": "c81af598a4bbdabcb575",
+    "url": "/static/js/runtime-main.44e80c72.js"
+  }
+]);
